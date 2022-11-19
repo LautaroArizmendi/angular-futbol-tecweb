88716680a623679c04805fb23df7d6f38a4c5d8c
Lautaro Arizmendi <lautaroarizmendi@gmail.com>
2022-11-19 17:18:58 -0300

    App changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePlayerComponent } from './create-player/create-player.component';
 import { PlayersComponent } from './players-list/players-list.component';
 import { TeamsComponent } from './teams-list/teams-list.component';
 
 const routes: Routes = [
-  {path: '',redirectTo: '/teams',pathMatch: 'full' },
+  { path: '',redirectTo: '/teams',pathMatch: 'full' },
   { path: 'teams', component: TeamsComponent },
   { path: 'players', component: PlayersComponent },
+  { path: 'players/new', component: CreatePlayerComponent },
+  { path: 'players/:id/update', component: CreatePlayerComponent }
  ];
  
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayersComponent } from './players-list/players-list.component';
 import { TeamsComponent } from './teams-list/teams-list.component';
+import { CreatePlayerComponent } from './create-player/create-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayersComponent,
     TeamsComponent,
+    CreatePlayerComponent,
   ],
   imports: [
     BrowserModule,
