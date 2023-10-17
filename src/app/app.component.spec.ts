73a67503707f617ed83a7e77c8cf5ed60b5ac32b
Lautaro Arizmendi <lautaroarizmendi@gmail.com>
2023-10-17 00:13:28 -0300

    Tittle's things deleted

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,17 +20,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'futbol'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('futbol');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('futbol app is running!');
-  });
 });
